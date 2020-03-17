c1ad2c8c06df109bab281b78c25a659f946f4008
aq_minato.naka <minato.naka@asia-quest.jp>
2020-03-17 12:13:22 +0900

    ユーザseeder作成

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\Models\User::class)->create([
+            'name' => 'naka',
+            'email' => 'minato.naka@asia-quest.jp',
+        ]);
+
+        factory(\App\Models\User::class, 10)->create();
+    }
+}
