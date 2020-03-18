2e760831303c1229bf98e62b5778dc5e727743c2
aq_minato.naka <minato.naka@asia-quest.jp>
2020-03-18 15:18:18 +0900

    laravel-breadcrumbsをインストール、適用

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('breadcrumbs')
+    {{ Breadcrumbs::render('home') }}
+@endsection
+
 @section('main')
     <div class="container-fluid">
         <div id="ui-view">
