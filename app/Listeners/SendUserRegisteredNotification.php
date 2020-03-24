7d11b0ec0c2b5467b41ce54bc3d1370dc8ca727a
aq_minato.naka <minato.naka@asia-quest.jp>
2020-03-24 17:14:31 +0900

    ユーザ登録のリスナ名修正

--- app/Listeners/SendUserRegisteredNotification.php
+++ app/Listeners/SendUserRegisteredNotification.php
@@ -5,7 +5,7 @@ namespace App\Listeners;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
-class UserRegisteredNotification
+class SendUserRegisteredNotification
 {
     /**
      * Create the event listener.
