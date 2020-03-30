c7272a6f519705d6961e19f426f2834f10af4197
aq_minato.naka <minato.naka@asia-quest.jp>
2020-03-30 10:29:57 +0900

    laravel-permissionのconfig,migrationをpublish

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Kyslik\ColumnSortable\ColumnSortableServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
